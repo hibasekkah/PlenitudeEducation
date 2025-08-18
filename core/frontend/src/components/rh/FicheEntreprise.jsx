e0d714be15cd29a181386f9acbe92ede90246d93
hibasekkah <157165724+hibasekkah@users.noreply.github.com>
2025-08-18 14:16:34 +0100

    fiche entreprise

--- core/frontend/src/components/rh/FicheEntreprise.jsx
+++ core/frontend/src/components/rh/FicheEntreprise.jsx
@@ -1,7 +1,27 @@
-
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export function FicheEntreprise(){
 
-    return <> fiche d'entreprise
+    return <>
+        <Card >
+        <CardHeader>
+            <CardTitle>fiche d'entreprise</CardTitle>
+            <CardDescription>les inforamtions de l'entreprise</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+            <p>Card Footer</p>
+        </CardFooter>
+        </Card>
     </>
 }
